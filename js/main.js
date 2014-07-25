90e467c7ce234ec1d0d8e8517a6a62356e42dd32
beforeadream <valery.lyatsevich@sandsiv.com>
2014-07-25 18:59:39 +0300

    configured requirejs-angular

--- js/main.js
+++ js/main.js
@@ -3,11 +3,17 @@ require.config({
     baseUrl: 'js',
     paths: {
         'jquery': 'bower_components/jquery/jquery.min',
-        'uikit': 'bower_components/uikit/dist/js/uikit.min'
+        'uikit': 'bower_components/uikit/dist/js/uikit.min',
+        'angular': 'bower_components/angular/angular.min',
+        'angular-route': 'bower_components/angular-route/angular-route.min',
+        'angularAMD': 'bower_components/angularAMD/angularAMD.min'
     },
     shim: {
         'uikit': {
             deps: ['jquery']
-        }
-    }
+        },
+        'angularAMD': ['angular'],
+        'angular-route': ['angular']
+    },
+    deps: ['app']
 });

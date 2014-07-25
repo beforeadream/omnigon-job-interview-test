caf5eadaf2f077d6e547ecb4f7993febbb8012d1
beforeadream <valery.lyatsevich@sandsiv.com>
2014-07-25 19:21:45 +0300

    created promise

--- controllers/start.js
+++ controllers/start.js
@@ -2,6 +2,15 @@ define(['js/app'], function (app) {
     'use strict';
 
     app.register.controller('startCtrl', ['$scope', '$http', function ($scope, $http) {
+        var url = 'http://api.massrelevance.com/reccosxof/matchtrax_hashclash_featured_tweets.json';
+        var responsePromise = $http.get(url);
 
+        responsePromise.success(function(data) {
+            console.log(data)
+        });
+
+        responsePromise.error(function(data, status, headers, config) {
+            console.log('fail');
+        });
     }]);
 });
